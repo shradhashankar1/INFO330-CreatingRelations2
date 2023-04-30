2e94ccacce4111f74445d465074608ac5ff63f54
shradhashankar1 <115043260+shradhashankar1@users.noreply.github.com>
2023-04-29 23:52:20 -0700

    Create ExtraCred1.sql

---
+++ ExtraCred1.sql
@@ -0,0 +1,4 @@
+INSERT INTO Pokemon (pokedex_number, name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed)
+VALUES (101, 'Huskichu', 'Mascot', NULL, 200, 150, 150, 200, 200, 200);
+INSERT INTO Pokemon (pokedex_number, name, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed)
+VALUES (102, 'Cougarite', 'Mascot', NULL, 10, 0, 0, 0, 0, 0);
